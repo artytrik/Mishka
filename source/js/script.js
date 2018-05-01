42951c96f9fa717d87072126f32b60041e601ce4
artytrik <tema-luch@mail.ru>
2018-05-01 11:29:05 +1000

    Сделал модальное окно

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,11 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
+var modalOverlay = document.querySelector('.modal-overlay');
+
+var orderLink = document.querySelector('.best__order');
+var orderPopup = document.querySelector('.modal-cart');
+
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
@@ -12,4 +17,16 @@ navToggle.addEventListener('click', function() {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
+})
+
+orderLink.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  orderPopup.classList.add('modal-cart__show');
+  modalOverlay.classList.add('modal-overlay__show');
+})
+
+modalOverlay.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  orderPopup.classList.remove('modal-cart__show');
+  modalOverlay.classList.remove('modal-overlay__show');
 })
